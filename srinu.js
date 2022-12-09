4dc1ab15a5783a5db7b23f1a2447ae436127b714
MeSrinu <balisrinu1996@gmail.com>
2022-12-09 02:07:01 -0800

    alssfugyb

---
+++ srinu.js
@@ -0,0 +1,18 @@
+
+function test() {
+  let arr = [1, 2, 3, 4, 34, 89];
+
+
+  for (let i = 0; i < arr.length; i++) {
+    return arr[i]
+  }
+
+  console.log(arr[i]);
+
+}
+
+
+setTimeout(test, 1000);
+
+
+// setTimeout(console.log("srinuVasuDev"), 10000)
